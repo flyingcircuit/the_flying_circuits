0e623005fe233228655ef670ca3f713cecfc040c
flyingcircuit <43682096+flyingcircuit@users.noreply.github.com>
2018-10-07 15:27:53 +0100

    Delete email.py

--- email.py
+++
@@ -1,7 +0,0 @@
-def emailvalidator():
-    email = input('Please type your email here:')
-    if (len(email) > 15 and email.count('@') == 1 and email.find('.gov.uk') != -1):
-        print ('Thank you for your email')
-    else:
-        print ('Sorry this is not a valid input') 
-        emailvalidator()
